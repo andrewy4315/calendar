7f35b379220d11fa31b5033cf29c433187ed042b
andrewy4315 <andrewy4315@gmail.com>
2024-03-19 03:42:00 -0400

    remove bcrypt

--- src/Server.js
+++ src/Server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const bcrypt = require('bcrypt');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
